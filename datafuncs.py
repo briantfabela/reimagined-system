f8a829f6b451b147d07a96863247759084236384
Edward Lee <46063222+EdwardLeeData@users.noreply.github.com>
2019-07-07 14:47:11 -0700

    Update datafuncs.py

--- datafuncs.py
+++ datafuncs.py
@@ -45,3 +45,22 @@ def count_entries(csv_file, c_size, colname):
 
     # Return counts_dict
     return counts_dict
+
+
+
+# Define read_large_file()
+def read_large_file(file_object):
+    """A generator function to read a large file lazily."""
+
+    # Loop indefinitely until the end of the file
+    while True:
+
+        # Read a line from the file: data
+        data = file_object.readline()
+
+        # Break if this is the end of the file
+        if not data:
+            break
+
+        # Yield the line of data
+        yield data
